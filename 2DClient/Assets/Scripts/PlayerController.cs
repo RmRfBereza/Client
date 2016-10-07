6f87ce2a6204a67a263c5fcc6812b4e200740453
Maxim Zemskov <m.zemskov1@gmail.com>
2016-10-07 13:19:36 +0300

    Теперь повороты происходят по тестовым ивентам

--- 2DClient/Assets/Scripts/PlayerController.cs
+++ 2DClient/Assets/Scripts/PlayerController.cs
@@ -1,95 +1,78 @@
 ﻿using UnityEngine;
-using System.Collections;
-
-//Adding this allows us to access members of the UI namespace including Text.
-using UnityEngine.UI;
-using Random = System.Random;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;             //Floating point variable to store the player's movement speed.
-
-    private Rigidbody2D rb2d;		//Store a reference to the Rigidbody2D component required to use 2D Physics.
-
-    private Vector2 newPosition;
-    private Random rand = new Random();
+    private Vector3 newDirection;
     private double timeleft;
 
-    // Use this for initialization
+    private float speed = 0.1f;
+
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D>();
-        rb2d.position = Vector2.zero;
+        transform.position = Vector2.zero;
 
-        timeleft = 1;
+        //в тестовом варианте по умолчанию игрок движется влево
+        newDirection = new Vector3(speed, 0);
     }
 
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        //следующий код предназначен для передвижения игрока стрелками. Будет выпилено позже. Сейчас оставил для тестов.
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce(movement * speed);
-
         //следующий код отвечает за передвежиние игрока по полченным координатам, через апи
-
-        if (timeleft < 0)
+        int checkNumber = CheckTurnEvent();
+        if (checkNumber != 0)
         {
-            newPosition = GetPlayerCoordTest();
-            timeleft = 1;
-            rb2d.position = newPosition;
+            newDirection = GetNewDirection(checkNumber);
         }
-        timeleft -= Time.deltaTime;
+        transform.Translate(newDirection);
     }
 
-    Vector2 GetPlayerCoordTest()
+    int CheckTurnEvent()
     {
-        float x = rb2d.position.x;
-        float y = rb2d.position.y;
+        //все(кроме последнего) условия проверяют находится ли игрок в углу, для того, чтобы вертеть его по кругу
+        if (transform.position.x >= 10.8 && transform.position.y >= 10.8)
+        {
+            return 1;
+        }
+        else if (transform.position.x <= -10.8 && transform.position.y >= 10.8)
+        {
+            return 2;
+        }
+        else if (transform.position.x <= -10.8 && transform.position.y <= -10.8)
+        {
+            return 3;
+        }
+        else if (transform.position.x >= 10.8 && transform.position.y <= -10.8)
+        {
+            return 4;
+        }
+        else if (transform.position.x >= 10.8)
+        {
+            return 5;
+        }
+        else
+        {
+            return 0;
+        }
+    }
 
-        var choice = rand.Next(0, 4);
-
-        //бросаю монету(возможные вариант 1 или 0)
-        if (choice == 0)
+    Vector3 GetNewDirection(int directionNumber)
+    {
+        //!TODO: после реализации API изменить метод, на реальную проверку ивентов.
+        switch (directionNumber)
         {
-            x += 3.6f;
-            y -= 3.6f;
+            case 1:
+                return new Vector3(-speed, 0);
+            case 2:
+                return new Vector3(0, -speed);
+            case 3:
+                return new Vector3(speed, 0);
+            case 4:
+                return new Vector3(0, speed);
+            case 5:
+                return new Vector3(0, speed);
+            default:
+                return newDirection;
         }
-        else if(choice == 1)
-        {
-            x -= 3.6f;
-            y += 3.6f;
-        }
-        else if (choice == 2)
-        {
-            x += 3.6f;
-            y += 3.6f;
-        }
-        else if (choice == 3)
-        {
-            x -= 3.6f;
-            y -= 3.6f;
-        }
-
-        if (x > 10.8 || x < -10.8f)
-        {
-            x = 0f;
-        }
-        if (y > 10.8 || y < -10.8f)
-        {
-            y = 0f;
-        }
-        return new Vector2(x, y);
     }
 }
